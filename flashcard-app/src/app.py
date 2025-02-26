4b5d73329130c4e6d97224330e4a12780b41ae8b
Advait Sepuri <90327626+adsepuri35@users.noreply.github.com>
2025-02-26 16:08:17 -0500

    sample tkinter gui

--- flashcard-app/src/app.py
+++ flashcard-app/src/app.py
@@ -1,20 +1,32 @@
-import gspread
-from oauth2client.service_account import ServiceAccountCredentials
+import tkinter as tk
+from tkinter import messagebox
 
-# test google sheets api
-def get_first_cell(sheet_url):
-    scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
-    creds = ServiceAccountCredentials.from_json_keyfile_name('../config/credentials.json')
-    client = gspread.authorize(creds)
+def on_button_click():
+    user_input = entry.get()
+    if user_input:
+        messagebox.showinfo("Information", f"You entered: {user_input}")
+    else:
+        messagebox.showwarning("Input Error", "Please enter something")
 
-    sheet1 = client.open_by_url(sheet_url).sheet1
-    first_cell = sheet1.cell(2,1).value
-    return first_cell
-
+# Main function to create the GUI
 def main():
-    sheet_url = 'https://docs.google.com/spreadsheets/d/1A1lxacaMp3v9_S1K6dCCNyqq0icE0gBoMtr1Ro4jeVA/edit?gid=0#gid=0'
-    first_cell_value = get_first_cell(sheet_url)
-    print(first_cell_value)
+    root = tk.Tk()
+    root.title("Chinese Flashcard App")
+    root.geometry("1000x500")
+
+    large_font = ("Helvetica", 16)
+
+    label = tk.Label(root, text="What lessons are you reviewing? (ex. 1, 4-8, 13)", font=large_font)
+    label.pack(pady=10)
+
+    global entry
+    entry = tk.Entry(root, width=50, font=large_font)
+    entry.pack(pady=10)
+
+    button = tk.Button(root, text="Submit", command=on_button_click, font=large_font)
+    button.pack(pady=10)
+
+    root.mainloop()
 
 if __name__ == "__main__":
     main()
